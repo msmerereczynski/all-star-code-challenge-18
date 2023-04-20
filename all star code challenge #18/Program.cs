886b1ea94653194f22325042c36074868635601d
m.smereczynski <m.smereczynski@H91004336>
2023-04-20 10:32:28 +0200

    First fully working version

--- all star code challenge #18/Program.cs
+++ all star code challenge #18/Program.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace all_star_code_challenge__18
 {
-    internal class Program
+    public class Program
     {
+        public static string str;
+        public static char letter;
+        
+        public static int StrCount(string str, char letter)
+        {
+            int count = 0;
+            foreach (var c in str)
+            {
+                if (c == letter)
+                {
+                    count++;
+                }
+            }
+            return count; //Do Some Magic
+        }
         static void Main(string[] args)
         {
-            //to jest testowy komentarz
+            Console.WriteLine("Please give me the word!: ");
+            str = Console.ReadLine();
+            Console.WriteLine("Please give me the letter!: ");
+            //try
+            //{
+                letter = Convert.ToChar(Console.ReadLine());
+            //}
+            //catch {
+            //    Console.WriteLine("Please ");
+            //}
+            int count = StrCount(str, letter);
+            Console.WriteLine($"Amount of '{letter}' in '{str}' is '{count}'");
+            Console.ReadKey();
         }
     }
 }
